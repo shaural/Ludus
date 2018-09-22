5316701e4a9267e763d1a6cb7bbbc558bf04e8c3
Alex Willis <willis62@purdue.edu>
2018-09-22 18:50:18 -0400

    Begin accounts creation

---
+++ functions/accounts.js
@@ -0,0 +1,51 @@
+```js
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const database = admin.database().ref('/users');
+
+//needed for request parsing
+var bodyParser = require('body-parser');
+
+
+
+
+exports.myEndpoint = functions.https.onRequest((request, response) => {
+    return cors(request, response, () => {
+         switch (request.method) {
+            case(POST):
+                //parse JSON fields
+                var inputName = request.body.name
+                var hashedPassword = request.body.password
+                var inputEmail = request.body.email
+
+                //not sure if the right way to save DOB
+                var inputDob = request.body.dob
+
+
+                //TODO: Validate data before making object
+                database.set({
+                    inputName:{
+                        Name = inputName,
+                        password = hashedPassword,
+                        email = inputEmail,
+                        DOB = inputDob
+                    }
+                });
+
+                return res.status(200).json({
+                    message: 'User created'
+                })
+
+
+            default:
+                // unsupported method
+                return res.status(405).json({
+                    message: 'Method not allowed!'
+                });
+
+
+         }
+        }
+    }
+`js;
