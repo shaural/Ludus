34e59f3c9003bf1c644261a7e46a7af01ec1f7d3
Alex Willis <willis62@purdue.edu>
2018-09-22 20:41:48 -0400

    Minor additions for accounts

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-
+exports.accounts = require('./myEndpoint').myEndpoint;
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
