99dda4eade0fa05186530232a82a2fbf35a0ae9f
Alex Willis <willis62@purdue.edu>
2018-09-26 00:58:58 -0400

    Learning Path creation works

--- functions/learning_path.js
+++ functions/learning_path.js
@@ -2,70 +2,75 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 
-function validate_input(id,topic, name,owner ){
-    if(id == "undefined" || topic == "undefined" || name == "undefined" || owner == "undefined"){
+function validate_input(topic, name,owner ){
+    if(topic === undefined || name === undefined || owner === undefined){
+        console.log("undefined data")
         return false;
     }
-    if(!id.toString().length || !topic.toString().length || name.toString().length || owner.toString().length){
+    else if(topic.toString().length == 0 || name.toString().length == 0 || owner.toString().length == 0){
+         console.log("zero-length string")
         return false;
     }
     return true;
 }
 
 exports.create_learning_path = functions.https.onRequest((request, response) => {
-
     return cors(request, response, () => {
         switch (request.method) {
             case('POST'):
                 const {
-                    lpid,
+                    // lpid,
                     topic,
-                    name,
+                    lpname,
                     owner,
-                    Class_List = null,
-                    Students_Enrolled = null,
+
+                    //NOTE: URL will be hardcoded for testing purposes, change the variable
+                    //as needed
+                    //in production code, this part will be called by the frontend page
+                    //which then will pass in our actual URL
+                    url = "https://ludus.com/users/12313122/teacher/testteacher",
+
+                    //for now, these parts will be null until a class gets added
+                    //to a learning path
+                    ClassList = null,
+                    StudentsEnrolled = null,
                     Teachers_who_recommend = null
 
                 } = request.body
-            }
-            console.log("Body " + request.body)
-            console.log(request.headers)
-            // console.log(name, owner)
-            console.log(lpid, name, Owner)
-            if(validate_input(lpid,topic,name,owner) == false){
-                return response.status(400).json({
-                    message: 'Something went wrong, undefined data was passed in!'
-                })
-            }
+                console.log(lpname, topic,owner)
+                if(validate_input(topic,lpname,owner) == false){
+                    return response.status(400).json({
+                        message: 'Something went wrong, undefined data was passed in!'
+                    })
+                }
 
             //parse url to find users id and teacher
-            let url = request.url.toString()
+            //this part will be unused, for now
             let url_tokens = url.split('/')
             let user_id = null;
             let teacher = null;
-                for(var i = 0; i < url_tokens.length(); i++){
+                for(var i = 0; i < url_tokens.length; i++){
                     if(url_tokens[i] == 'users'){
                         user_id = url_tokens[i+1];
                     }
                     else if(url_tokens[i] == 'teacher'){
                         teacher = url_tokens[i+1];
+                        break;
                     }
                 }
                 
-            const database = admin.database().ref('/Users/' + user_id +'/' + teacher + '/' + learningPaths);
-            
+                const database = admin.database().ref('/Learning_Paths');
                 database.set({
-                        'ID':lpid,
                         'Topic': topic,
-                        'Name': name,
+                        'Name': lpname,
                         'Owner': owner,
                         'Class_List': null,
-                        'Students_Enrolled': null,
-                        'Teachers_who_recommend': null
+                        'St_Enrolled': null,
+                        'T_recommend': null
                 })
                 return response.status(200).json({
-                    message: 'Creating new learning path!'
+                    message: 'Created a new learning path!'
                 })
-
+            }
             })
         })
