21f728299d9015f2f0e3d93230af3aed609ee1b1
Alex Willis <willis62@purdue.edu>
2018-09-27 23:43:43 -0400

    Remove bad folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import 'react-bootstrap';
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: ''
+    };
+  }
+
+  getValidationState() {
+    const isEmpty = this.state.value.length;
+    if (isEmpty == 0) {
+      return 'bad data';
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,9 +33,25 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <form>
+          <FormGroup
+            name="Enter name"
+            validationState={this.getValidationState()}
+          >
+            <ControlLabel>Working example with validation</ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.value}
+              placeholder="Enter text"
+              onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+            <HelpBlock>Validation is based on string length.</HelpBlock>
+          </FormGroup>
+        </form>
       </div>
     );
   }
 }
-
+render(<FormExample />);
 export default App;
