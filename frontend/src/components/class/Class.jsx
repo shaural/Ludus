f41450816c64b3035d628995e0a43bac9c6cc8a3
Lumenebrae <lumenebrae@gmail.com>
2018-09-30 19:17:22 -0400

    tidying up 2

--- frontend/src/components/class/Class.jsx
+++ frontend/src/components/class/Class.jsx
@@ -5,24 +5,23 @@ class Class extends Component {
   render() { 
     return (
       <div>
-        {this.fetchData(this.props.classID)}
+        { this.fetchData( this.props.classID ) }
       </div>
     );
   }
 
-    fetchData(ClassID) {
-        //TODO: API Call for info on the class from database
-        return (
-            <container>
-                <span className="ClassInfo">
-                  <span>{/* name */ "Example Class 101"}  LP: {/* lp */ "ExamplePath"}  Content Type: {/* ct */ "ExampleType"}  </span> 
-                  <div className="Rating"> {/* rating */"Example"}/5 </div>
-                  <div> Tags: {/* rating */"ExampleTags"} Comments: {/* Comments */ "ExampleComments"} </div>
-                </span> 
-                
-            </container>
-        );
-    }
+  fetchData(ClassID) {
+    //TODO: API Call for info on the class from database
+    return (
+      <container>
+        <span className="ClassInfo">
+          <span>{ /*Placeholder values for now*/ "Example Class 101" }  LP: { "ExamplePath" }  Content Type: { "ExampleType" } </span> 
+          <div className="Rating"> { "Example" }/5 </div>
+          <div> Tags: { "ExampleTags" } Comments: { "ExampleComments" } </div>
+        </span> 
+      </container>
+    );
+  }
 
 }
  
--- frontend/src/components/class/Classlist.jsx
+++ frontend/src/components/class/Classlist.jsx
@@ -3,23 +3,20 @@ import Class from './Class';
 import ClassRemoveButton from './ClassRemoveButton';
 import ClassEditButton from './ClassEditButton';
 import './Classlist.css';
-//Need some array, to store ClassID's fetched from the database using the userID
-
 
 class Classlist extends Component {
   createClasslist = () => {
     //let classIDList = []
     //TODO: Call API for classIDs
     let classes = []
-    
-    for (let i = 0; i < 17; i++) {
+
+    //for (let i = 0; i < classIDList.length; i++) {
+    for ( let i = 0; i < 17; i++ ) {
       classes.push(
-      <div  className="ClassObject">
-        {<Class classID = {i /* classIDList[k] */} />} 
+      <div className="ClassObject">
+        { <Class classID = { i /* classIDList[i] */ } /> } 
         <span className="Highlight">
-          {<ClassEditButton />}
-          <text>{" "}</text>
-          {<ClassRemoveButton />}
+          { <ClassEditButton /> } <text>{" "}</text> { <ClassRemoveButton /> }
         </span>
       </div>
       );
@@ -30,9 +27,9 @@ class Classlist extends Component {
 
   render() {
     return (
-        <div className="Classlist">
-          {this.createClasslist()}
-        </div>
+      <div className="Classlist">
+        { this.createClasslist() }
+      </div>
     );
   }
 
