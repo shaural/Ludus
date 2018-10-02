32f69d65461e8540016408bd224c68993c2b6212
ShauralP <patel675@purdue.edu>
2018-10-01 21:47:18 -0400

    auto-format

--- functions/users.js
+++ functions/users.js
@@ -140,13 +140,19 @@ app.delete('/:user_id', async (request, response) => {
   userref
     .remove()
     .then(function() {
-      return response.status(200)
-      .json({ message: `User with id ${request.params.user_id} deleted.` });
+      return response
+        .status(200)
+        .json({ message: `User with id ${request.params.user_id} deleted.` });
     })
     .catch(function(error) {
       console.log('Error deleting user:', error);
-      return response.status(400)
-        .json({ message: `Error, Could not delete user with id ${request.params.user_id}` });
+      return response
+        .status(400)
+        .json({
+          message: `Error, Could not delete user with id ${
+            request.params.user_id
+          }`
+        });
     });
 });
 
