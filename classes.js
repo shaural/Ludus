37196ac154a24820c8541fffb83116a49a023d4b
shen282 <shen282@purdue.edu>
2018-10-03 16:45:01 -0400

    classes update API

---
+++ classes.js
@@ -0,0 +1,34 @@
+const admin = require('./utils').firebaseAdmin;
+
+const app = require('express')();
+app.use(require('cors')({ origin: true }));
+
+app.patch('/:classes/class_id', async (request, response) => {
+  const db = admin.database().ref(`/classes/${request.params.class_id}`);
+  if (!db)
+    return response.status(404).json({
+      message: `class with id ${request.params.class_id} not found`
+    });
+
+  const { name, rating, content_type, owner, tags, comments } = request.body;
+  
+    await db
+      .push({
+        Name: name,
+        Owner: owner,
+        Ratings: rating || [],
+        Content_type: content_type || [],
+        Tags: tags || [],
+        Comments: comments || []
+      })
+      .once('value').then(snapshot => {
+        resp = {
+          id: snapshot.key,
+          class: { ...snapshot.val() }
+        };
+      });
+
+  return response.status(200).json(resp);
+});
+
+exports.route = app;
