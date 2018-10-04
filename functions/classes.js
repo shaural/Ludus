7b93a4a5b009aaea8218ba7ad3628c6fcbe41e64
Alex Brooke <akb.sbc@gmail.com>
2018-10-04 12:26:40 -0400

    add method to delete class

--- functions/classes.js
+++ functions/classes.js
@@ -11,24 +11,48 @@ app.patch('/classes/:class_id', async (request, response) => {
     });
 
   const { name, rating, content_type, owner, tags, comments } = request.body;
-  
-    await db
-      .push({
-        Name: name,
-        Owner: owner,
-        Ratings: rating || [],
-        Content_type: content_type || [],
-        Tags: tags || [],
-        Comments: comments || []
-      })
-      .once('value').then(snapshot => {
-        resp = {
-          id: snapshot.key,
-          class: { ...snapshot.val() }
-        };
-      });
+
+  await db
+    .push({
+      Name: name,
+      Owner: owner,
+      Ratings: rating || [],
+      Content_type: content_type || [],
+      Tags: tags || [],
+      Comments: comments || []
+    })
+    .once('value')
+    .then(snapshot => {
+      resp = {
+        id: snapshot.key,
+        class: { ...snapshot.val() }
+      };
+    });
 
   return response.status(200).json(resp);
+});
+
+app.delete('/classes/:class_id', async (request, response) => {
+  const db = admin.database.ref(`/classes/${request.params.class_id}`);
+  if (!db)
+    return response.status(404).json({
+      message: `class with id ${request.params.id} not found`
+    });
+  return db
+    .remove()
+    .then(() =>
+      response
+        .status(200)
+        .json({ message: `class with id ${request.params.class_id} deleted` })
+    )
+    .catch(err =>
+      response
+        .status(400)
+        .json({
+          message: `could not delete class with id ${request.params.class_id}`,
+          err
+        })
+    );
 });
 
 exports.route = app;
