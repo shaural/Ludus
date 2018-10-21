74d1ac063a7bbf74cf2931159ae5f2d91535031a
Lumenebrae <lumenebrae@gmail.com>
2018-10-21 15:30:21 -0400

    Applied changes to input handling

--- frontend/src/components/account/SignUp.jsx
+++ frontend/src/components/account/SignUp.jsx
@@ -1,48 +1,72 @@
 import React, { Component } from 'react';
 import './SignUp.css';
+var querystring = require('querystring');
 const Axios = require('axios');
 
 class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayName: '', email: '', password: '', dob: '' };
+    this.submitForm = this.submitForm.bind(this);
+  }
+
   render() {
     return (
       <form className="SignUpForm">
         Display Name:&nbsp;
-        <input type="text" id="displayname" />
+        <input
+          type="text"
+          onChange={event => this.setState({ displayName: event.target.value })}
+        />
         <br /> <br />
         Email:&nbsp;
         {'\t\t'}
-        <input type="text" id="email" />
+        <input
+          type="text"
+          onChange={event => this.setState({ email: event.target.value })}
+        />
         <br /> <br />
         Password:&nbsp;
-        <input type="password" id="password" />
+        <input
+          type="password"
+          onChange={event => this.setState({ password: event.target.value })}
+        />
         <br /> <br />
         {/*Confirm Password:&nbsp;*/}
         {/*<input id="passwordconfirm" /><br />*/}
         Date of Birth:&nbsp;
-        <input type="date" id="dob" />
+        <input
+          type="date"
+          onChange={event => this.setState({ dob: event.target.value })}
+        />
         <br /> <br />
-        <input type="submit" id="submitbutton" onClick={this.submitForm} />
+        <input type="button" id="submitbutton" onClick={this.submitForm} />
       </form>
     );
   }
 
   submitForm() {
-    Axios.post('https://us-central1-ludusfire.cloudfunctions.net/users/', {
-      data: {
-        name: document.getElementById('displayname').value,
-        password: document.getElementById('password').value,
-        email: document.getElementById('email').value,
-        dob: document.getElementById('dob').value
-      }
-    })
+    const requestBody = {
+      name: this.state.displayName,
+      password: this.state.password,
+      email: this.state.email,
+      dob: this.state.dob
+    };
+    const config = {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    };
+    Axios.post(
+      'https://us-central1-ludusfire.cloudfunctions.net/users/',
+      querystring.stringify(requestBody),
+      config
+    )
       .then(function(response) {
         console.log(response);
       })
       .catch(function(error) {
         console.log(error);
       });
-
-    return;
+    return false;
   }
 }
 
