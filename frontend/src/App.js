297ed26a7f3fe98b526e61192436f0ec54d00178
Alex Willis <willis62@purdue.edu>
2018-10-25 21:50:19 -0400

    Successfully tested password reset functionality...needs formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 // import custom component
 import { Login } from './login';
-
+import { PasswordReset } from './passwordreset/PasswordReset';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { link } from 'fs';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Ludus</h1>
-        </header>
-        <p className="App-intro">
-          Please enter your username and password to begin
-        </p>
+        {/*router handling for stuff like the password reset page */}
+        <BrowserRouter>
+          <div>
+            <div>
+              <Route path="/passwordreset" component={PasswordReset} exact />
+            </div>
 
-        {/* This is how you render a custom component */}
-        <Login />
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to Ludus</h1>
+            </header>
+            <p className="App-intro">
+              Please enter your username and password to begin
+            </p>
+
+            <li>
+              <Link to="/passwordreset">Reset Password</Link>
+            </li>
+
+            {/* This is how you render a custom component */}
+            <Login />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
