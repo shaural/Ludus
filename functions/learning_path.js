87c4c7535db444403810da89dc96de73e8793943
ShauralP <patel675@purdue.edu>
2018-10-29 20:28:33 -0400

    updated post class to lp method in learning_path.js to account for ordering

--- functions/learning_path.js
+++ functions/learning_path.js
@@ -3,30 +3,47 @@ const admin = require('./utils').firebaseAdmin;
 const app = require('express')();
 app.use(require('cors')({ origin: true }));
 
+// Body: Index: Order of class in learning path, class_id
 app.post('/:lp_id/class', async (request, response) => {
   const db = admin
     .database()
-    .ref(`/Learning_Paths/${request.params.lp_id}/Class`);
+    .ref(`/Learning_Paths/${request.params.lp_id}/Classes`);
+  
+  const { index, class_id } = request.body;
+  if(index < 0) {
+    // invalid (assuming index starting with 0)
+    return response.status(400).json({
+      message: `Invalid index.`
+    });
+  }
   if (!db)
     return response.status(404).json({
       message: `learning path with id ${request.params.lp_id} not found`
     });
 
   // TODO: append to learningpath/:lp_id/class array without creating UID structure?
-  const { class_id } = request.body;
   if (!class_id)
     return response.status(400).json({ message: 'class_id can not be empty' });
   try {
+    // check if index already taken (need to use PATCH instead)
+    let indexExists = false;
     await db
-      .push({
-        Class_Id: class_id
+      .once('value').then(snapshot => {
+        if(snapshot.hasChild(index)){
+          indexExists = true;
+        }
+      });
+    
+    if(indexExists) {
+      return response.status(400).json({ message: `Learning path already contains class at index ${index}. Please use PATCH method instead.` });
+    }
+    await db
+      .set({
+        index: class_id
       })
       .once('value')
       .then(snapshot => {
-        resp = {
-          id: snapshot.key,
-          class: { ...snapshot.val() }
-        };
+        resp = snapshot.val();
       });
   } catch (e) {
     return response.status(400).json({ message: 'malformed request' });
@@ -34,11 +51,11 @@ app.post('/:lp_id/class', async (request, response) => {
   return response.status(200).json(resp);
 });
 
-app.get('/:lp_id/class', (request, response) => {
+app.get('/:lp_id/classes', (request, response) => {
   //console.log('Ran new code');
   const db = admin
     .database()
-    .ref(`/Learning_Paths/${request.params.lp_id}/class`);
+    .ref(`/Learning_Paths/${request.params.lp_id}/Classes`);
   if (!db)
     return response
       .status(404)
