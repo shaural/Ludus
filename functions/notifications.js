f7e78ea12fc33148ae8558ca8d082c2f119e2691
Shaural Patel <patel675@purdue.edu>
2018-11-29 13:08:57 -0500

    created notificications.js and post method to send notificiation to user

---
+++ functions/notifications.js
@@ -0,0 +1,47 @@
+const admin = require('./utils').firebaseAdmin;
+const app = require('express')();
+app.use(require('cors')({ origin: true }));
+
+// Send notification to a User
+// Body: sender_name, text
+app.post('/:receiver_id', async (request, response) => {
+  const db = admin
+    .database()
+    .ref(`/Notificaitons/${request.params.receiver_id}`);
+  let receiver = request.params.receiver_id;
+  const { sender_name, text } = request.body;
+  console.log(sender_name);
+  console.log(text);
+  if (sender_name.length > 0 && text.length > 0) {
+    db.push({
+      sender_name: text
+    });
+    return response.status(200).json({
+      message: `Notification successfully sent.`
+    });
+
+    // //store notification into
+    //   await db
+    //   .once('value', snapshot => {
+    //     //push to receiver
+    //     db.push({
+    //       Name: tags[i]
+    //     });
+    //   });
+  } else {
+    //sender or text not provided
+    return response.status(400).json({
+      message: `Body arguments are either empty or invalid.`
+    });
+  }
+});
+
+// GET method (for all records)
+app.get('/', (request, response) => {
+  const ref = admin.database().ref('/Tags');
+  ref.once('value', function(snapshot) {
+    return response.status(200).json(snapshot.val());
+  });
+});
+
+exports.route = app;
