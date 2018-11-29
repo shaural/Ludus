9ab0cd97546a4bbe4b3ccf5d67c68308f9854074
Shaural Patel <patel675@purdue.edu>
2018-11-29 15:54:46 -0500

    fixed typo in post notifications and created get method to get all notifications for user

--- functions/notifications.js
+++ functions/notifications.js
@@ -7,11 +7,9 @@ app.use(require('cors')({ origin: true }));
 app.post('/:receiver_id', async (request, response) => {
   const db = admin
     .database()
-    .ref(`/Notificaitons/${request.params.receiver_id}`);
+    .ref(`/Notifications/${request.params.receiver_id}`);
   let receiver = request.params.receiver_id;
   const { sender_name, text } = request.body;
-  console.log(sender_name);
-  console.log(text);
   if (sender_name.length > 0 && text.length > 0) {
     db.push({
       sender_name: sender_name,
@@ -20,15 +18,6 @@ app.post('/:receiver_id', async (request, response) => {
     return response.status(200).json({
       message: `Notification successfully sent.`
     });
-
-    // //store notification into
-    //   await db
-    //   .once('value', snapshot => {
-    //     //push to receiver
-    //     db.push({
-    //       Name: tags[i]
-    //     });
-    //   });
   } else {
     //sender or text not provided
     return response.status(400).json({
@@ -37,11 +26,20 @@ app.post('/:receiver_id', async (request, response) => {
   }
 });
 
-// GET method (for all records)
-app.get('/', (request, response) => {
-  const ref = admin.database().ref('/Tags');
+// GET method (for all notitifications for user: user_id)
+app.get('/:user_id', (request, response) => {
+  const ref = admin.database().ref(`/Notifications/${request.params.user_id}`);
   ref.once('value', function(snapshot) {
-    return response.status(200).json(snapshot.val());
+    console.log(snapshot.val());
+    if (snapshot.hasChildren()) {
+      return response.status(200).json(snapshot.val());
+    } else {
+      return response.status(400).json({
+        message: `User with id: ${
+          request.params.user_id
+        } does not have any notifications.`
+      });
+    }
   });
 });
 
